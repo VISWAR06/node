e15643cb6a0d5d6f2d3d9ef00221c013a7c097a8
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-04-30 18:20:04 +0530

    req session url

--- App.js
+++ App.js
@@ -4,6 +4,15 @@
 // auto exit use process.exit()
 const https=require('http')
 const server=https.createServer((rep,res)=>{
+    const url=rep.url
+    if(url==='/'){
+        res.setHeader('content-type','text/html')
+        res.write('<html>')
+        res.write('<header><title>form</title></header>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><input type="submit" value="send"> </form></body>')
+        res.write('</html>')
+        return res.end()
+    }
     res.setHeader('Content-type','text/html')
     res.write('<html>')
     res.write('<header><title>practive</title></header>')
