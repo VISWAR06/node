e23fdc09c10f4545518b182dbd8f716c0c73e5be
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-04-30 18:06:42 +0530

    req session

--- App.js
+++ App.js
@@ -1,1 +1,14 @@
- console.log("hello")
+// const fs=require('fs')
+// fs.writeFileSync('hello.txt','hello from node.js')
+// rep.url,rep.menthod,rep.header
+// auto exit use process.exit()
+const https=require('http')
+const server=https.createServer((rep,res)=>{
+    res.setHeader('Content-type','text/html')
+    res.write('<html>')
+    res.write('<header><title>practive</title></header>')
+   res.write('<body><h1>nan than da leo!!</h1></body>')
+    res.write('</html>')
+    res.end()
+})
+server.listen(3000)
